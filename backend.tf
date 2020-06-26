08f899a70e7fb5e95141f2048b080a1621865f7b
EC2 Default User <ec2-user@ip-172-31-10-24.eu-west-2.compute.internal>
2020-06-26 19:19:30 +0000

    s3 bucket changed

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-april-class-kellysalrin"
+    bucket = "terraform-state-april-class-makpal"
     key    = "jenkins/us-east-1/tools/prometheus_grafana/prometheus_grafana.tfstate"
     region = "us-east-1"
   }
